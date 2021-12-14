df0f2abcc612c8a230c2c8141eb83eacacae6a45
Igor Cardoso <iygorsilva@gmail.com>
2021-12-14 11:56:42 -0300

    promise pt2

--- functional/13-promise_2.js
+++ functional/13-promise_2.js
@@ -1,22 +1,28 @@
-setTimeout(() => {
-    console.log('Running callback...')
+// setTimeout(() => {
+//     console.log('Running callback...')
 
-    setTimeout(() => {
-        console.log('Running callback...')
+//     setTimeout(() => {
+//         console.log('Running callback...')
 
-        setTimeout(function() {
-            console.log('Running callback...')
+//         setTimeout(function() {
+//             console.log('Running callback...')
 
-        }, 2000);
-    }, 2000);
-}, 2000);
+//         }, 2000);
+//     }, 2000);
+// }, 2000);
 
 
-function waitForI(time = 2000) {
-    return Promise(function(keepPromise) {
+function waitFor(time = 2000) {
+    return new Promise(function(keepPromise) {
         setTimeout(() => {
-            console.log('Running promise...')
-            keepPromise()
-        }, time);
+            console.log('Running promise...', time)
+            keepPromise('XXXX')
+        }, time)
     })
 }
+
+// Call the waitFor three times
+waitFor()
+        .then(() => waitFor(3000))
+        .then(() => waitFor())
+        // .then(v => console.log(v))
