1ae7f7a90ee86e449b39dec5b19b8e522377b4c5
Igor Cardoso <iygorsilva@gmail.com>
2021-12-14 09:44:03 -0300

    map - callback function

---
+++ functional/9-callback_3.js
@@ -0,0 +1,53 @@
+const numbers = [1, 2, 3, 4, 5, 6];
+const double = (n, i, a) => n * 2 + i + a.length;
+
+console.log(numbers.map(double))
+
+// (method) Array<number>.map<number>(callbackfn: (value:
+//     number, index: number, array: number[]) => number,
+//     thisArg?: any): number[]
+
+
+// Calls a defined callback function on each element of an array,
+// and returns an array that contains the results.
+
+// @param callbackfn — A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+
+// @param thisArg — An object to which the this keyword can refer
+// in the callbackfn function. If thisArg is omitted, undefined is
+// used as the this value.
+
+console.log('--------------------')
+
+loja = [
+    {name: 'Mesa', quantity: 2, price: 321.8},
+    {name: 'Cadeira', quantity: 8, price: 91.8},
+    {name: 'Notebook', quantity: 12, price: 2192.8},
+    {name: 'Cabo', quantity: 7, price: 1.8}
+];
+
+const getName = myItem => myItem.name;
+const getQuantity = myItem => myItem.quantity;
+const getPrice = myItem => myItem.price;
+const getTotal = myItem => myItem.price*myItem.quantity;
+
+const names = loja.map(getName);
+const prices = loja.map(getPrice);
+const quantity = loja.map(getQuantity);
+const total = loja.map(getTotal);
+
+console.log(names)
+console.log(prices)
+console.log(quantity)
+console.log(total)
+
+
+console.log('---------- My Map Function ----------')
+
+Array.prototype.myMap = function(fn) {
+    const newArray = []
+    for(let i = 0; i < thisArray.length; i++) {
+        result = fn(thisArray[i], i, thisArray)
+        newArray.push(result)
+    }
+}
