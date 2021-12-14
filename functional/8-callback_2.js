6c60a781bf0c7d50b3da2329d8d9fb97b6eeaa75
Igor Cardoso <iygorsilva@gmail.com>
2021-12-14 09:04:02 -0300

    callback function exemple p2

--- functional/8-callback_2.js
+++ functional/8-callback_2.js
@@ -1,13 +1,39 @@
 const fs = require('fs')
 const path = require('path')
 
-const file_path = path.join(__dirname, 'data.txt')
 
-showContent = (error, content) => {
+// ### File read with callback function ###
+
+// The __dirname get the current path on the OS
+const file_path = path.join(__dirname, 'data.txt')
+console.log('File path: ', file_path)
+
+function showContent(_, content) {
     console.log(content.toString())
 }
+console.log('Start...')
+
+// The readFile function receives the path as options and the callback
+// Don't wait for the file to read
+fs.readFile(file_path, {}, showContent) // or fs.readFile(file_path, showContent)
+fs.readFile(file_path, (_, data) => console.log(data.toString()))
+
+console.log('End...')
 
 
-fs.readFile(file_path, {}, showContent)
+// ******  readFile ******
+// function readFile(path: fs.PathOrFileDescriptor, options: {
+//     encoding?: null;
+//     flag?: string;
+// } & EventEmitter.Abortable, callback: (err: NodeJS.ErrnoException, data: Buffer) => void): void (+3 overloads)
+// namespace readFile
 
-console.log(file_path)
+
+console.log('\n-------------------------------\n')
+// ### File read no callback function ###
+console.log('Start Sync...')
+
+const contentSyncFile = fs.readFileSync(file_path)
+console.log(contentSyncFile.toString())
+
+console.log('End Sync...')
