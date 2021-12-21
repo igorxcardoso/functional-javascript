bc471f2d9ceb3019ac0217343272b69149a9556d
Igor Cardoso <iygorsilva@gmail.com>
2021-12-20 22:29:06 -0300

    finish challenge

--- functional/challenge_2.js
+++ functional/challenge_2.js
@@ -1,14 +1,22 @@
 const fs = require('fs')
 const path = require('path')
-const file_path = path.join(__dirname, 'data.txt')
 
 
-function readMyFile() {
-    return new Promise (fs.readFile(file_path, {}, (_, content) => {
-        console.log(content.toString())
-    }
-
-    ));
+function readMyFile(file_path) {
+    return new Promise(resolve => {
+        // Processo assíncrono
+        fs.readFile(file_path, {}, function(_, content){   // Poderia ser uma arrow-function
+            resolve(content.toString());
+        });
+        console.log('Read late!');
+    });
 }
 
-readMyFile.then()
+const caminhoArquivo = path.join(__dirname, 'data.txt')
+// readMyFile(caminhoArquivo).then(conteudo => console.log(conteudo))
+readMyFile(caminhoArquivo)
+    .then(conteudo => conteudo.split('\n'))
+    // .then(linhas => linhas.length())
+    .then(juntar => juntar.join(', '))
+    .then(conteudo => `The final values is: ${conteudo}`)
+    .then(console.log);
